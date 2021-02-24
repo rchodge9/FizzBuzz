ff7f9660818eb2f3b4744a16fd5a10f91b344eda
Rachel Hodge <rchodge9@gmail.com>
2021-02-24 17:24:12 -0600

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Program
+
+    {
+
+        /* Your challenge:
+
+        Write a public method that accepts a number
+
+        when the number is divisible by 3 return the word fizz
+
+        when the number is divisible by 5 return the word buzz
+
+        when the number is divisible by both, return the word fizzbuzz*/
+
+
+
+
+        public static string FizzBuzz(int x)
+        { 
+            if (x%3 ==0 && x%5==0)
+            {
+                return "fizzbuzz";
+       
+            }
+            else if (x%5==0)
+            {
+                return "buzz";
+
+            }
+            else if (x%3==0)
+            {
+                return "fizz";
+            }
+            else
+            {
+                return "neither";
+            }
+        }
+        static void Main (string[]args)
+        {
+           
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(FizzBuzz (x));
+        }
+           
+
+
+        
+
+           
+            
+
+
+        
+
+            
+        
+
+    
+    }
+}
